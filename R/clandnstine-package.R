0488e9001c5000cc772b24d4ff5face5a419c9da
hrbrmstr <bob@rud.is>
2019-01-18 12:53:33 -0500

    R package repo initialization complete

---
+++ R/clandnstine-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/clandnstine>
+#' - BugReports: <https://gitlab.com/hrbrmstr/clandnstine/issues>
+#' 
+#' @md
+#' @name clandnstine
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
